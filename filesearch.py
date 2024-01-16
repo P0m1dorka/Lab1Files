3fd91a9a478091d81df0827b024518173afb422e
P0m1dorka <loginovandrej31@gmail.com>
2024-01-16 19:21:01 +0300

    add base for search_file

--- filesearch.py
+++ filesearch.py
@@ -1,1 +1,13 @@
 import pathlib
+
+
+def search_files(dir:str, name:str, inDirDir:bool = 0, rash:str = " ", name_rash:str= ""):
+    print(")___(")
+    path = pathlib.Path(dir)
+    files = list()
+    for child in path.glob(name+'.*'):
+        print(child)
+    print("Hello world")
+
+
+
