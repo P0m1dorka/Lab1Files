8d66d7511b8b008eb78b0f9d2a101be0979fa457
SomebodyAroundThere <156821622+SomebodyAroundThere@users.noreply.github.com>
2024-01-22 15:37:55 +0300

    Update filesearch.py

--- filesearch.py
+++ filesearch.py
@@ -7,7 +7,17 @@ def search_files(dir:str, name:str, inDirDir:bool = 0, rash:str = " ", name_rash
     files = list()
     for child in path.glob(name+'.*'):
         print(child)
-    print("Hello world")
+    print("Hello world") 
+
+searching_directory = pathlib.Path()       
+def search_dir(dirname_part: str, path: searching_directory, recursive: bool):
+    directories = []
+    if recursive:
+        for directory in Path(path).rglob(f"{dirname_part}"):
+            directories.append(directory) 
+    elif not recursive:
+        for directory in Path(path).glob(f"{dirname_part}"):
+            directories.append(directory)
 
 
 
